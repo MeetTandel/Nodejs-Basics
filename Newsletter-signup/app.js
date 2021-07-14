06d017bc49fd16c2ba17b4c271c1431eee1dd157
MeetTandel <meettandel47@gmail.com>
2021-07-14 09:57:43 +0530

    added Newsletter

---
+++ Newsletter-signup/app.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const app = express();
+const https = require("https");
+const bodyParser = require("body-parser")
+// const request = require("request");
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/signup.html")
+})
+
+app.post("/", function (req, res) {
+    const firstName = req.body.fname;
+    const lastName = req.body.lname;
+    const Email = req.body.email;
+
+    const data = {
+        members: [
+            {
+                email_address: Email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    }
+    const jsonData = JSON.stringify(data);
+    const url = "https://us1.api.mailchimp.com/3.0/lists/c58e414f55";
+
+    const options = {
+        method: "POST",
+        auth: "Meet1:ff8687318d9afea30618403306ad7323-us1"
+    }
+     const request = https.request(url, options, function(response){
+
+        if(response.statusCode == 200){
+            res.sendFile(__dirname + "/success.html")
+        }
+        else{
+            res.sendFile(__dirname + "/failure.html");
+        }
+        response.on("data", function(data){
+            console.log(JSON.parse(data));
+        });
+    });
+    request.write(jsonData);
+    request.end();
+});
+
+app.post("/failure", function(req, res){
+    res.redirect("/");
+})
+
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log("Server is running of port ", 3000)
+})
+
+//Unique Id = c58e414f55
+
+//Api Key = ff8687318d9afea30618403306ad7323-us1
